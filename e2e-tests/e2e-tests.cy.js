485b32ef8bed0cc5bf9271413ffb5928c3c8e9ec
timohuu <timo.huuhtanen@hotmail.fi>
2024-08-22 14:01:41 +0300

    11.9 Simple end-to-end tests run#3

--- e2e-tests/e2e-tests.cy.js
+++ e2e-tests/e2e-tests.cy.js
@@ -7,5 +7,15 @@ describe('Pokedex', function() {
     //cy.visit('http://localhost:8080')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  }),
+  it('Pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:5000')
+    //cy.visit('http://localhost:8080')
+    //cy.contains('ivysaur')
+    //cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+    cy.contains('ivysaur')
+      .click()
+    cy.contains('overgrow')
+    cy.contains('chlorophyll')
   })
 })
