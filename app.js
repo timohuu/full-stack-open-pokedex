8b2d695802665e7deb7a94584409aaa7e42c693e
timohuu <115079893+timohuu@users.noreply.github.com>
2024-08-26 12:27:18 +0300

    Revert update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 
 // get the port from env variable
-//const PORT = process.env.PORT || 5000
-const PORT = process.env.PORT || 10000
+const PORT = process.env.PORT || 5000
+//const PORT = process.env.PORT || 10000
 
 app.use(express.static('dist'))
 
