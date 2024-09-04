9fc5e0f3398a9df791cc5dc27d8fc918f9c65a22
timohuu <timo.huuhtanen@hotmail.fi>
2024-09-04 13:32:56 +0300

    11.12 Health check: simulating broken deployment

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
-// just some change to trigger the deployment
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
